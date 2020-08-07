499df648c8df90ff810e6ecd464d71cbda13a11e
Rebecka Skareng <ogustafsson1@gmail.com>
2020-08-07 12:49:27 +0200

    Added linter

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,6 @@
 // Initial welcome page. Delete the following line to remove it.
-let body = document.getElementsByTagName("body")[0]; 
+const body = document.getElementsByTagName('body')[0];
 
-let header = document.createElement("h1");
-header.innerText = "En header";
+const header = document.createElement('h1');
+header.innerText = 'En header';
 body.appendChild(header);
