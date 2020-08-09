d757658d0e38b7fb3d82d2e81e34b6e4ace4360d
ozzyian <ogustafsson1@gmail.com>
2020-08-09 20:51:29 +0200

    Added functionality to match color scheme

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,10 @@
 // Initial welcome page. Delete the following line to remove it.
 const body = document.getElementsByTagName('body')[0];
 
+if (window.matchMedia('(prefers-color-scheme:dark)').matches) {
+  body.style.color = 'white';
+}
+
 const header = document.createElement('h1');
 header.innerText = 'En header';
 body.appendChild(header);
